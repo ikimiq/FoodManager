eccf87f764d0154dc03e1b7e3f932d0e4385b61b
ikimiq <66920440+ikimiq@users.noreply.github.com>
2020-09-11 23:21:12 +0700

    Update FoodManager.py

--- FoodManager.py
+++ FoodManager.py
@@ -3,37 +3,38 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-wb = openpyxl.load_workbook(filename='food.xlsx')
-wb.active = 0
+wb = openpyxl.load_workbook(filename='food.xlsx') #открытие файла для чтения
+wb.active = 0 #установка рабочего листа на первый
 sheet = wb.active
-wb_output = openpyxl.Workbook()
-wb_output.active = 0
+wb_output = openpyxl.Workbook() #создание и открытие файла для записи
+wb_output.active = 0 #установка рабочего листа на первый
 sheet_output = wb_output.active
-sheet_output['A1'] = 'Заказ, с учетом остатков'
+sheet_output['A1'] = 'Заказ, с учетом остатков' # на первый столбец таблицы записывается эта фраза
 for i in range(2, 6):
-	sheet_output['A' + str(i)] = sheet['B'+str(i)].value - sheet['C'+str(i)].value
+	sheet_output['A' + str(i)] = sheet['B'+str(i)].value - sheet['C'+str(i)].value #заносится в столбец A разница между заказом и остатком
 
 
 msg = MIMEMultipart()
-message = "Thank you"
-password = "kingking2281337"
-msg['From'] = "testtesttesttest9990@gmail.com"
-msg['To'] = "nekrylov1618@gmail.com"
-msg['Subject'] = "privet"
+message = "Thank you" #сообщение которое отправялется
+password = "kingking2281337" # пароль от почты с которой идет отправление
+msg['From'] = "testtesttesttest9990@gmail.com" #почта с которой отправляется
+msg['To'] = "nekrylov1618@gmail.com" # почта куда отправляется
+msg['Subject'] = "privet" # тема сообщения
 
 msg.attach(MIMEText(message, 'plain'))
 
-server = smtplib.SMTP('smtp.gmail.com: 587')
- 
-server.starttls()
+server = smtplib.SMTP('smtp.gmail.com: 587') # подключение к серверу отправки сообщений
 
-server.login(msg['From'], password)
+server.starttls() # хз точно, но как понял делает подключение к почте безопасным
 
-server.sendmail(msg['From'], msg['To'], msg.as_string())
- 
+server.login(msg['From'], password) #логин в почту с которой будет отправляться сообщение
+
+server.sendmail(msg['From'], msg['To'], msg.as_string()) # отправка сообщения
+
 server.quit()
- 
-print ("successfully sent email to %s:" % (msg['To']))
 
-wb.save('food.xlsx')
-wb_output.save('food1.xlsx')
+print ("successfully sent email to %s:" % (msg['To'])) # вывод того что сообщение отправилось
+
+wb.save('food.xlsx') #закрытие файла из которго читали
+wb_output.save('food1.xlsx') # закрытие файла в который запсиывали
+
