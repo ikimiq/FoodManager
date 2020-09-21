3eaa2a8e5bb5ece526209879a6b47a26c302edb8
Kirill-99 <66934622+Kirill-99@users.noreply.github.com>
2020-09-21 20:12:09 +0700

    Update blok.mailmessage.py

--- blok.mailmessage.py
+++ blok.mailmessage.py
@@ -26,3 +26,4 @@ server.quit()
 # Ссылки на инструкции:
 # https://defpython.ru/otpravka_poczty_s_pomosczu_smtplib - простая настройка
 # http://codius.ru/articles/Python_%D0%9A%D0%B0%D0%BA_%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C_%D0%BF%D0%B8%D1%81%D1%8C%D0%BC%D0%BE_%D0%BD%D0%B0_%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D1%83%D1%8E_%D0%BF%D0%BE%D1%87%D1%82%D1%83 - сложно и не понятно
+# http://python-3.ru/page/imap-email-python - принимаем и анализируем письма
